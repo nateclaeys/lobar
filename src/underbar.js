84fc07f842cf2932230f1c18d9f5489b9aacf542
nrandall <nrandall82@gmail.com>
2016-04-02 12:10:05 -0700

    Completed map

--- src/underbar.js
+++ src/underbar.js
@@ -80,16 +80,33 @@
 
   // Return all elements of an array that pass a truth test.
   _.filter = function(collection, test) {
+    var results = [];
+    _.each(collection, function(item, i){
+      if (test(item)){
+        results.push(item);
+      }
+    });
+    return results;
   };
 
   // Return all elements of an array that don't pass a truth test.
   _.reject = function(collection, test) {
     // TIP: see if you can re-use _.filter() here, without simply
     // copying code in and modifying it
+    return _.filter(collection, function(item){
+      return !(test(item));
+    });
   };
 
   // Produce a duplicate-free version of the array.
   _.uniq = function(array) {
+    var results = [];
+    _.each(array, function(item, i){
+      if (_.indexOf(results, item) < 0){
+        results.push(item);
+      }
+    });
+    return results;
   };
 
 
@@ -98,6 +115,11 @@
     // map() is a useful primitive iteration function that works a lot
     // like each(), but in addition to running the operation on all
     // the members, it also maintains an array of results.
+    var results = [];
+    _.each(collection, function(item){
+      results.push(iterator(item));
+    });
+    return results;
   };
 
   /*
@@ -139,6 +161,7 @@
   //   }); // should be 5, regardless of the iterator function passed in
   //          No accumulator is given so the first element is used.
   _.reduce = function(collection, iterator, accumulator) {
+    
   };
 
   // Determine if the array or object contains a given value (using `===`).
